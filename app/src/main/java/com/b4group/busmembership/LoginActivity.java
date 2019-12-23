4fc470329c8a862575607ff9f260830663f6aaae
shreyas <shreyas.mahangade@hostingduty.com>
2019-12-23 18:49:46 +0530

    Added API in LoginScreen

--- app/src/main/java/com/b4group/busmembership/LoginActivity.java
+++ app/src/main/java/com/b4group/busmembership/LoginActivity.java
@@ -6,8 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,6 +47,44 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    public String api_call(int pull,int bus_id, double latitude, double longitude){
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String api_string = pull == 1 ? "pull_coordinates" : "push_coordinates";
+//        api_string += "?bus_id=" + bus_id;
+//        api_string += "&x=" + latitude;
+//        api_string += "&y=" + longitude;
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, Global.base_url+api_string,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        //textView.setText("Response is: "+ response.substring(0,500));
+//                        Log.i("Json Response",response.toString());
+//                        try {
+//                            JSONArray jArray = new JSONArray(response);
+//                            for (int i=0;i<jArray.length();i++){
+//                                JSONObject obj = jArray.getJSONObject(i);
+//                                globalLatLng = new LatLng(obj.getDouble("x"),obj.getDouble("y"));
+//                                Log.i("Json Response",obj.toString());
+//                            }
+//                        } catch (JSONException e) {
+//                            Log.e("Json Response",e.toString());
+//                        }
+//
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                //textView.setText("That didn't work!");
+//                Log.e("API Response", "Error in API Request" + error.toString());
+//            }
+//        });
+
+// Add the request to the RequestQueue.
+//        queue.add(stringRequest);
+        return "";
     }
 
 }
